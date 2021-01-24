58b0bfc7a218d084eaae44c0e57988486c4fd521
TheerapakG <theerapakg@gmail.com>
2021-01-24 19:31:31 +0700

    add grid to common

--- tgraphics/elements/common/__init__.py
+++ tgraphics/elements/common/__init__.py
@@ -2,3 +2,4 @@ from .mixin import *
 from .button import Button, ButtonBGDefault, ButtonState, ButtonType, LabelButton
 from .empty import Empty
 from .grid import Grid, StaticGrid, StaticGridError, StructuredGrid, StructuredStaticGrid, Subelement
+from . import grid

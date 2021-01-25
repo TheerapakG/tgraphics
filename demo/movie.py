2f5893fa075bd15a79673211ebf364e0238ba7b5
TheerapakG <theerapakg@gmail.com>
2021-01-25 07:57:45 +0700

    move windows shit from demo to lib

--- demo/movie.py
+++ demo/movie.py
@@ -1,21 +1,5 @@
 import os
 from pathlib import Path
-import sys
-
-# Windows shit
-if sys.platform.startswith('win'):
-    import ctypes
-    _fpath = None
-    for _path in os.environ['Path'].split(os.pathsep):
-        path = Path(_path)
-        if (path/'ffmpeg.exe').exists():
-            _fpath = path
-            break
-        elif (path/'bin'/'ffmpeg.exe').exists():
-            _fpath = path/'bin'
-            break
-    if _fpath:
-        ctypes.windll.kernel32.SetDllDirectoryW(str(_fpath))
 
 import tgraphics
 
--- tgraphics/backend/pygame/media_backend/pyglet.py
+++ tgraphics/backend/pygame/media_backend/pyglet.py
@@ -1,9 +1,26 @@
 import datetime
 import pyglet
 from pyglet.gl import *
+import os
+from pathlib import Path
+import sys
 import time
-
 from ..pygame import Texture, _current_renderer
+
+# Windows shit
+if sys.platform.startswith('win'):
+    import ctypes
+    _fpath = None
+    for _path in os.environ['Path'].split(os.pathsep):
+        path = Path(_path)
+        if (path/'ffmpeg.exe').exists():
+            _fpath = path
+            break
+        elif (path/'bin'/'ffmpeg.exe').exists():
+            _fpath = path/'bin'
+            break
+    if _fpath:
+        ctypes.windll.kernel32.SetDllDirectoryW(str(_fpath))
 
 class Player:
     def __init__(self):
